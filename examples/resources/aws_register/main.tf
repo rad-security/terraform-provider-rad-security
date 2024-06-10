15446488bbbecd51e232a47b556a23bc9f8dbb59
Max Brain <max.brain@ksoc.com>
2024-06-10 12:48:48 +0200

    Updates examples
    Signed-off-by: Max Brain <max.brain@ksoc.com>

--- examples/resources/aws_register/main.tf
+++ examples/resources/aws_register/main.tf
@@ -1,4 +1,4 @@
-resource "rad_security_aws_register" "this" {
+resource "rad-security_aws_register" "this" {
   rad_security_assumed_role_arn = "arn:aws:iam::<aws_account_number>:role/rad-security-connector"
   aws_account_id        = "aws_account_id"
 }
